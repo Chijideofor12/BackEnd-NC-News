9814b7783d4c0b4229f78fb7cdf9794e8893be88
Chika Jideofor <pammyjideofor1@hotmail.co.uk>
2025-02-07 00:38:56 +0000

    add endpoint documentation for GET /api/users/:username

--- models/users.model.js
+++ models/users.model.js
@@ -6,5 +6,21 @@ const getAllUsers = () => {
       return rows;
     });
 };
+const selectUserByUsername = (username) =>{
+  const SQLString = `
+  SELECT 
+  *
+  FROM
+    users
+     WHERE username = $1;
+  `;
+  return db.query(SQLString, [username])
+  .then(({ rows }) => {
+    if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Username not found" });
+    }
+    return rows[0];
+});
+}
 
-module.exports = { getAllUsers };
+module.exports = { getAllUsers, selectUserByUsername };
